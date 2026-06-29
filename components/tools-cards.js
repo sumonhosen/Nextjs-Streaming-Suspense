202b6615602c3d55dbd371235acc59128e9c4d16
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2026-06-29 16:27:48 +0600

    streaming suspense with skeleton issues fix

--- components/tools-cards.js
+++ components/tools-cards.js
@@ -1,16 +1,24 @@
-
-import IconCard from "@/components/icon-card";
 import getTools from "@/lib/getTools";
+import { Suspense, use } from "react";
+import IconCard from "./icon-card";
+import CardSkeleton from "./ui/card-skeleton";
+
+const ToolsCard = ({ toolPromise }) => {
+    const tool = use(toolPromise);
+
+    return <IconCard tool={tool} />;
+};
 
 const ToolsCards = async () => {
-    const tools = await getTools();
-    const toolsWithData = await Promise.all(tools);
+    const toolsPromise = await getTools();
 
     return (
         <div className="w-full max-w-4xl mx-auto px-4 sm:px-6">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-6 py-6">
-                {toolsWithData.map((tool) => (
-                    <IconCard key={tool} tool={tool} />
+                {toolsPromise.map((toolPromise, index) => (
+                    <Suspense fallback={<CardSkeleton />} key={index}>
+                        <ToolsCard toolPromise={toolPromise} />
+                    </Suspense>
                 ))}
             </div>
         </div>
